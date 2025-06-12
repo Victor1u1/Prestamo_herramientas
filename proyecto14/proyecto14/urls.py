1aa7fcaa36607d16f65ec200a9e4c60550759ece
cheloph <chelopherrera@gmail.com>
2025-06-11 23:19:06 -0400

    url imagenes

--- proyecto14/proyecto14/urls.py
+++ proyecto14/proyecto14/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
 
 
 urlpatterns = [
