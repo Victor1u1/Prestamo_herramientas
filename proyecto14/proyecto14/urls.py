e21da20e8f3398e4bb4e33166a2b2bbbbad695ee
cheloph <chelopherrera@gmail.com>
2025-06-11 23:19:31 -0400

    url imagenes

--- proyecto14/proyecto14/urls.py
+++ proyecto14/proyecto14/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from django.conf.urls.static import static
-
+from django.conf import settings
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
